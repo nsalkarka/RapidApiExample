ef9d64e373e1ed92c5f60e75d412fd193b49fb30
nsalkarka <_karka67@windowslive.com>
2025-03-02 22:51:19 +0300

    Add project files.

---
+++ RapidApi.WebUI/Controllers/GasController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApi.WebUI.Models;
+
+namespace RapidApi.WebUI.Controllers
+{
+    public class GasController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://gas-price.p.rapidapi.com/europeanCountries"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a41975009dmsh8e32d64062a5468p1445ebjsnd7ef0058dc43" },
+        { "x-rapidapi-host", "gas-price.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values=JsonConvert.DeserializeObject<GasPriceViewModel>(body);
+                return View(values.results);
+            }
+
+        }
+    }
+}
---
+++ RapidApi.WebUI/Controllers/ImdbController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RapidApi.WebUI.Models;
+
+namespace RapidApi.WebUI.Controllers
+{
+    public class ImdbController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://imdb236.p.rapidapi.com/imdb/top250-movies"),
+                Headers =
+    {
+        { "x-rapidapi-key", "a41975009dmsh8e32d64062a5468p1445ebjsnd7ef0058dc43" },
+        { "x-rapidapi-host", "imdb236.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var jsonbody = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ImdbViewModel.Movie>>(jsonbody);
+
+                var dramaList = values.Where(x => x.genres.Contains("Drama")).ToList();
+                return View(dramaList);
+            }
+
+        }
+    }
+}
